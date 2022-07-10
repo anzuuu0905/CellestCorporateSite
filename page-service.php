8b4b93847c290414eed18699a3d38f2901914209
anzuuu0905 <anzuuu@yahoo.co.jp>
2022-07-10 16:28:10 +0900

    ・serviceページ作成 ・全体のレスポンシブ対応見直し

---
+++ page-service.php
@@ -0,0 +1,128 @@
+<?php get_header(); ?>
+<div class="p-mv__titlebox">
+  <h1 class="p-mv__sub-title">
+    Service
+  </h1> 
+  <p class="p-mv__jp-title">
+  事業紹介
+  </p>
+</div>
+</section>
+
+<!-- Service -->
+<section class="l-sub-service p-sub-service">
+  <!-- <div class=""> -->
+    <div class="p-sub-service__wrapper l-inner">
+      <div class="p-sub-service__textwrapper">
+        <h2 class="p-sub-service__title c-common-title">
+        ABOUT LIVE COMMERCE
+        </h2>
+        <p class="p-sub-service__subtitle c-common-subtitle">
+          ネットショッピングで<br>
+          イメージ通りの買い物ができる
+        </p>
+        <div class="p-sub-service__text u-hidden-sp">
+          <p class="c-common-text">
+          ライブ配信とEコマース (ネットショップ)を組み合わせた言葉。言葉の通りライブコマースの仕組みは 「ライブ配信×ネットショップ」であり、商品購入は配信されたライブ動画を見ながら行うことができます。<br>
+          従来のネットショップでは画像と文章から商品の良し悪しを判断するしかなく、「思っていた大きさじゃなかった」「文章を読んでイメージした質感と違う」などと購入者を落胆させてしまう事例も多くありました。それを解決できる方法として編み出されたのがライブコマースです。
+          </p>
+        </div>
+      </div>
+      <div class="p-sub-service__img">
+        <picture>
+          <source srcset="<?php echo get_template_directory_uri() ?>/assets/img/common/sub-live-commerce-pc.png"
+                  media="(min-width: 768px)">
+          <img src="<?php echo get_template_directory_uri() ?>/assets/img/common/sub-live-commerce-sp.png" alt="" />
+        </picture>
+      </div>
+      <div class="p-sub-service__text u-hidden-pc">
+        <p class="c-common-text">
+          ライブ配信とEコマース (ネットショップ)を組み合わせた言葉。<br class="u-hidden-sp">
+          言葉の通りライブコマースの仕組みは 「ライブ配信×ネットショップ」であり、商品購入は配信されたライブ動画を見ながら行うことができます。<br>
+          従来のネットショップでは画像と文章から商品の良し悪しを判断するしかなく、「思っていた大きさじゃなかった」「文章を読んでイメージした質感と違う」などと購入者を落胆させてしまう事例も多くありました。それを解決できる方法として編み出されたのがライブコマースです。
+        </p>
+      </div>
+    </div>
+    <div class="p-sub-service__contents">
+      <div class="l-inner">
+        <h2 class="p-sub-service__contentstitle c-common-title">
+        Service
+        </h2>
+        <div class="p-sub-service__items">
+          <div class="p-sub-service__item p-sub-service-item">
+            <div class="p-sub-service-item__imgbox">
+              <picture>
+                <source srcset="<?php echo get_template_directory_uri() ?>/assets/img/common/sub-service1-pc.png"
+                        media="(min-width: 768px)">
+                <img class="p-sub-service-item__img" src="<?php echo get_template_directory_uri() ?>/assets/img/common/sub-service1-sp.png" alt="" />
+              </picture>
+            </div>
+            <!-- <p class="p-sub-service-item__no">01</p> -->
+            <div class="p-sub-service-item__textbox">
+              <p class="p-sub-service-item__entitle">
+              LIVE COMMERCE<br>
+              SALES BUSINESS
+              </p>
+              <h3 class="p-sub-service-item__title">
+              ライブコマース販売事業
+              </h3>
+              <div class="p-sub-service-item__text">
+                <p class="c-common-text">
+                  Saas型のライブコマースとSNSを活用して、「ぞうねこちゃんねる（旧ぞうとねこ）」という名前でライブコマース販売事業を行っております。取扱商材はアパレル・雑貨・食品・コスメと幅広く、自社ブランド「ZOUTONEKO」のアパレル商品、その他国内外のセレクト商品を販売しております。ライブコマーサーは内製化しているので、販売は外部委託ではなく、自社で育成・販売しており、企業様から販売やPRのご依頼もいただいております。
+                </p>
+              </div>
+              <div class="p-sub-service-item__result p-sub-service-result">
+                <p class="p-sub-service-result__title">実績</p>
+                <ul class="p-sub-service-result__list c-common-text">
+                  <li>5年のライブコマースキャリア</li>
+                  <li>月間1000万円以上の販売</li>
+                  <li>数々の配信イベントで大差をつけて1位獲得</li>
+                  <li>渋谷スクランブル交差点、道頓堀、上野アメ横の大型ビジョンに掲載</li>
+                </ul>
+              </div>
+              <div class="p-sub-service-item__iconbox">
+                <a href="" class="">
+                <img src="<?php echo get_template_directory_uri() ?>/assets/img/common/icon-instagram.png" alt="" />
+                </a>
+                <a href="" class="">
+                <img src="<?php echo get_template_directory_uri() ?>/assets/img/common/icon-tiktok.png" alt="" />
+                </a>
+                <a href="" class="">
+                <img src="<?php echo get_template_directory_uri() ?>/assets/img/common/icon-sales.png" alt="" />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="p-sub-service__item p-sub-service-item">
+            <picture>
+              <source srcset="<?php echo get_template_directory_uri() ?>/assets/img/common/sub-service2-pc.png"
+                      media="(min-width: 768px)">
+              <img class="p-sub-service-item__img" src="<?php echo get_template_directory_uri() ?>/assets/img/common/sub-service2-sp.png" alt="" />
+            </picture>
+            <!-- <p class="p-sub-service-item__no">02</p> -->
+            <div class="p-sub-service-item__textbox">
+              <p class="p-sub-service-item__entitle">
+              LIVE COMMERCE<br>
+              SALES BUSINESS
+              </p>
+              <h3 class="p-sub-service-item__title">
+              ライブコマースエージェント事務所
+              </h3>
+              <div class="p-sub-service-item__text">
+                <p class="c-common-text">
+                ライブコマースを始めるにあたって、何から始めていいかわからない、やっているけどうまく売上を伸ばせない、さらに事業拡大したいなどの需要が数多くある中、ライブコマースはまだ市場が浅く、こういった悩みの解決策は調べてもほとんど出てこないのが現状です。圧倒的な実績とノウハウをもった「セレスト」なら全てのお悩みを解決できる自信があります。個人、法人向けにライブコマーサーの育成やライブコマース事業部の立ち上げを支援しております。ライブコマースで収益を上げて、拡大していくために必要なコンテンツを提供します。
+                </p>
+              </div>
+              <!-- ボタン -->
+              <div class="p-sub-service-item__btn c-btn">
+                <a href="<?php echo $about ?>">VIEW MORE</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  <!-- </div> -->
+</section>
+
+<?php get_footer(); ?>
